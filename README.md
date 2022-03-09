c32d5352117eeeee73f81ed6ca45c0f179494eb4
gh <gh4777@naver.com>
2022-03-09 22:08:10 +0900

    base template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # svelte-snowpack
+
+3. SCSS와 Autoprefixer 구성.
+4. 넘어감
+5. 경로 별칭과 빌드 최적확 구성.
---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+const sp = require('svelte-preprocess')
+const auto = require('autoprefixer')
+
+
+module.exports = {
+    mount: {
+        public: '/',
+        src: '/_dist_'
+    },
+    plugins: [
+        ['@snowpack/plugin-svelte', {
+            preprocess: sp({
+                scss: {
+                    prependData: '@import "./src/scss/main.scss";'
+                },
+                postcss: {
+                    plugins: [
+                        auto()
+                    ]
+                }
+            })
+        }],
+        '@snowpack/plugin-optimize'
+    ],
+    alias: {
+        '~' : './src'
+    }
+}
